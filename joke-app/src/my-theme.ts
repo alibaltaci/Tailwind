b0d73e4d9e56700ee59b7a470df8e8261193cc0b
alibaltaci <alibaltaci13@gmail.com>
2022-07-13 01:45:34 +0300

    src

---
+++ joke-app/src/my-theme.ts
@@ -0,0 +1,16 @@
+// my-theme.ts
+import { DefaultTheme } from 'styled-components';
+
+const myTheme: DefaultTheme = {
+  borderRadius: '5px',
+
+  colors: {
+    main: "#3C12FA",
+    secondary: "#8A28DE",
+    white: "#fff",
+    purple: "#53239C",
+    green: "#00ff00",
+  },
+};
+
+export { myTheme };
