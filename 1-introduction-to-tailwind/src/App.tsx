f7d9e2f25722910565a8c9d67fe586ae5fbfa45d
ali baltaci <alibaltaci13@gmail.com>
2024-04-03 17:17:37 +0300

    end of first lesson

--- 1-introduction-to-tailwind/src/App.tsx
+++ 1-introduction-to-tailwind/src/App.tsx
@@ -1,7 +1,14 @@
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <a href="#" className="btn bg-indigo-200 hover:bg-indigo-700">
+        Button-1
+      </a>
+      <a href="#" className="btn bg-red-200 hover:bg-red-700">
+        Button-2
+      </a>
+    </>
   )
 }
 
